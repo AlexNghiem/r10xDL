12e8532938b2b48ba050844489a50181b453288e
AlexNghiem <alexandernghiem@gmail.com>
2019-06-21 08:19:14 -0700

    Initial File Input

---
+++ R/test_function_add_one.R
@@ -0,0 +1,12 @@
+#' A Test Function
+#'
+#' This function adds one to a number.
+#' @param number The number to add 1 to. Defaults to 7
+#' @keywords test
+#' @export
+#' @examples
+#' test_function_add_one()
+
+test_function_add_one <- function(number = 7) {
+  print(number + 1)
+}
