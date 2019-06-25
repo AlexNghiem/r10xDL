0a3555e7186f7fc9ada408b9239a62129371b943
AlexNghiem <alexandernghiem@gmail.com>
2019-06-25 11:35:10 -0700

    June 25 AM
    Edits from morning of June 25. Mostly working on analyzing 10x data set (work mostly in DeepLearning.Rmd)

---
+++ R/cleanSCE.R
@@ -0,0 +1,17 @@
+#' Cleans a SCE to remove all cells with zero libraries
+#'
+#' This function takes an SCE, removes all cells which have no library (no non-zero entries in its column), and returns the SCE. Used to clean the SCE for later processing
+#' @param sce the sce to clean
+#' @keywords sce clean
+#' @export
+#' @examples
+#' cleanSCE()
+
+cleanSCE <- function(sce) {
+  helper <- function(i) {
+    counts1 = counts(sce)[,i]
+    any(counts1 != 0)
+  }
+  nonzero = unlist(lapply(1:ncol(sce), helper))
+  sce[,nonzero]
+}
