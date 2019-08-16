e4ec2960c14302aa3df034887257d1fc31627d26
AlexNghiem <alexandernghiem@gmail.com>
2019-08-16 12:38:54 -0700

    commit before cruise:wq

--- code/Preparing2/0reading.R
+++ code/Preparing2/0reading.R
@@ -7,19 +7,23 @@ library(DropletUtils)
 data <- readRDS("10Xdata/faustResults.rds")
 
 sce <- read10xCounts("10Xdata/vdj_v1_hs_aggregated_donor1_filtered_feature_bc_matrix.h5")
-#sce$Sample <- "donor1"
-# 
-# sceTemp <- read10xCounts("10Xdata/vdj_v1_hs_aggregated_donor2_filtered_feature_bc_matrix.h5")
-# #sceTemp$Sample <- "donor2"
-# sce <- cbind(sce, sceTemp)
-# 
-# sceTemp <- read10xCounts("10Xdata/vdj_v1_hs_aggregated_donor3_filtered_feature_bc_matrix.h5")
-# #sceTemp$Sample <- "donor3"
-# sce <- cbind(sce, sceTemp)
-# 
-# sceTemp <- read10xCounts("10Xdata/vdj_v1_hs_aggregated_donor4_filtered_feature_bc_matrix.h5")
-# #sceTemp$Sample <- "donor4"
-# sce <- cbind(sce, sceTemp)
+sce$Sample <- "donor1"
+sce$color <- "red"
+
+sceTemp <- read10xCounts("10Xdata/vdj_v1_hs_aggregated_donor2_filtered_feature_bc_matrix.h5")
+sceTemp$Sample <- "donor2"
+sceTemp$color <- "yellow"
+sce <- cbind(sce, sceTemp)
+
+sceTemp <- read10xCounts("10Xdata/vdj_v1_hs_aggregated_donor3_filtered_feature_bc_matrix.h5")
+sceTemp$Sample <- "donor3"
+sceTemp$color <- "green"
+sce <- cbind(sce, sceTemp)
+
+sceTemp <- read10xCounts("10Xdata/vdj_v1_hs_aggregated_donor4_filtered_feature_bc_matrix.h5")
+sceTemp$Sample <- "donor4"
+sceTemp$color <- "blue"
+sce <- cbind(sce, sceTemp)
 
 
 
